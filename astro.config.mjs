56b376529d4817f937c4efb3fb8ae2f0f6274050
Sagar Velagala <sagar.velagala@gmail.com>
2025-07-13 18:08:02 -0400

    fix 2?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sagarv.xyz",
-  integrations: [],
+  integrations: [astro],
 });
